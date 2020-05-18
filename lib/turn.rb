38b55790a4ab9ef219beb59cccb91d0ce4e97d4d
Warda Ayaz <wayaz107@gmail.com>
2020-05-18 23:52:53 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,59 @@
+def display_board(board)
+  puts " # | # | # "
+  puts "-----------"
+  puts " # | # | # "
+  puts "-----------"
+  puts " # | # | # "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def player_move(board, index, marker)
+ board[index] = marker
+end
+
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else
+    return false
+  end
+end
+
+
+
+def current_player(board)
+  turn_count(board) % 2 == 0? "X" : "O"
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    player_move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  }
+  counter
+end
